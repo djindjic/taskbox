f82b6ad9895e5e7e239d8d0405d05ca522bc4287
Aleksandar Djindjic <djindjic@gmail.com>
2021-11-06 19:33:56 +0100

    initial commit
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-inbox-screen></app-inbox-screen>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'taskbox';
+}
